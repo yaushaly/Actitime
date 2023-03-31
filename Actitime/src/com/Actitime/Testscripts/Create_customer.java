ce83aec13daa7e45b7d4161605997fe711abdbb4
kaush <kaush@LAPTOP-NJDO0OL6>
2023-03-31 12:16:36 +0530

    kaushalsecondcommit

--- Actitime/src/com/Actitime/Testscripts/Create_customer.java
+++ Actitime/src/com/Actitime/Testscripts/Create_customer.java
@@ -1,5 +1,30 @@
 package com.Actitime.Testscripts;
 
-public class Create_customer {
+import java.io.IOException;
 
-}
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Test;
+
+import com.Actitime.GenericLibrary.Baseclass;
+import com.Actitime.GenericLibrary.Filelibrary;
+import com.Actitime.pom.HomePage;
+import com.Actitime.pom.Taskpage;
+
+public class Create_customer extends Baseclass {
+	@Test
+	public void creatCustomer() throws EncryptedDocumentException, IOException {
+		HomePage hp = new HomePage(driver);
+		hp.getTasklnk().click();
+		
+		Taskpage tp=new Taskpage(driver);
+		tp.getAddnumber().click();
+		tp.getNewcust().click();
+	 Filelibrary f = new Filelibrary();
+	String name = f.readDataFromExcelFile("Sheet1", 3, 1);
+	    tp.getCustname().sendKeys(name);
+	  String desp = f.readDataFromExcelFile("Sheet1", 3, 2);
+	   
+	   tp.getCustdescription().sendKeys(desp);
+	 tp.getCreatcust().click();
+   }
+} 
